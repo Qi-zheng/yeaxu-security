b425959a893accdb1fbf71829063b103efd57b7f
seven <seven@DESKTOP-U7MIE1D>
2017-11-16 20:49:17 +0800

    2

---
+++ yeaxu-security-browser/src/main/java/com/yeaxu/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.yeaxu.security.browser;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin()//启用表单登录
+//		http.httpBasic()  //启用httpBasic登录
+			.and()
+			.authorizeRequests()
+			.anyRequest()
+			.authenticated();
+	}
+}
---
+++ yeaxu-security-browser/src/main/java/com/yeaxu/security/browser/MyUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.yeaxu.security.browser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyUserDetailsService implements UserDetailsService {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		logger.info("登录用户名：" + username);
+		//加了salt，同一个密码每次加密出来的串是不同的
+		String password = passwordEncoder.encode("123456");
+		logger.info("数据库密码是：" + password);
+		return new User("Seven", password,
+				//用户校验逻辑
+				true, true, true, true, 
+				AuthorityUtils.commaSeparatedStringToAuthorityList("ADMIN"));
+	}
+
+}
