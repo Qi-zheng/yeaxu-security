23f57ed5d9828a6009841b2ac506454eff36428d
pcitc <pcitc@pcitc-PC>
2017-11-17 13:27:41 +0800

    记住我功能开发

--- yeaxu-security-browser/src/main/java/com/yeaxu/security/browser/BrowserSecurityConfig.java
+++ yeaxu-security-browser/src/main/java/com/yeaxu/security/browser/BrowserSecurityConfig.java
@@ -1,15 +1,20 @@
 package com.yeaxu.security.browser;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import com.yeaxu.security.core.properties.SecurityProperties;
 import com.yeaxu.security.core.validate.code.ValidateCodeFilter;
@@ -27,9 +32,23 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private AuthenticationFailureHandler yeaxuAuthenticationFailureHandler;
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private DataSource dataSource;
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public PersistentTokenRepository persistentTokenRepository() {
+		JdbcTokenRepositoryImpl tokenRepository= new JdbcTokenRepositoryImpl();
+		tokenRepository.setDataSource(dataSource);
+//		tokenRepository.setCreateTableOnStartup(true);
+		return tokenRepository;
 	}
 	
 	@Override
@@ -45,6 +64,11 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter{
 			.loginProcessingUrl("/authentication/form") 
 			.successHandler(yeaxuAuthenticationSuccessHandler)  //设置自定义成功处理器
 			.failureHandler(yeaxuAuthenticationFailureHandler)
+			.and()
+			.rememberMe()
+			.tokenRepository(persistentTokenRepository())
+			.tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
+			.userDetailsService(userDetailsService)
 			.and()
 //		http.httpBasic()  //启用httpBasic登录
 			.authorizeRequests()
--- yeaxu-security-core/src/main/java/com/yeaxu/security/core/properties/BrowserProperties.java
+++ yeaxu-security-core/src/main/java/com/yeaxu/security/core/properties/BrowserProperties.java
@@ -4,6 +4,8 @@ public class BrowserProperties {
 	private String loginPage = "/yeaxu-signIn.html";//如果用户不指定登录页时的默认值
 	
 	private LoginType loginType = LoginType.JSON;
+	
+	private int rememberMeSeconds = 3600;
 
 	public String getLoginPage() {
 		return loginPage;
@@ -19,6 +21,14 @@ public class BrowserProperties {
 
 	public void setLoginType(LoginType loginType) {
 		this.loginType = loginType;
+	}
+
+	public int getRememberMeSeconds() {
+		return rememberMeSeconds;
+	}
+
+	public void setRememberMeSeconds(int rememberMeSeconds) {
+		this.rememberMeSeconds = rememberMeSeconds;
 	}
 	
 }
