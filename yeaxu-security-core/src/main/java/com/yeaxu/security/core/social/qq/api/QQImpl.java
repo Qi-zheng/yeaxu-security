6b49862bfaef8a4d94cc7a453b874afd6530bed9
pcitc <pcitc@pcitc-PC>
2017-11-29 17:14:16 +0800

    QQ->Jdbc完成

--- yeaxu-security-core/src/main/java/com/yeaxu/security/core/social/qq/api/QQImpl.java
+++ yeaxu-security-core/src/main/java/com/yeaxu/security/core/social/qq/api/QQImpl.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class QQImpl extends AbstractOAuth2ApiBinding implements QQ {
 
 	private static final String URL_GET_OPENID = "https://graph.qq.com/oauth2.0/me?access_token=%s";
-	private static final String URL_GET_USER_INFO = "https://graph.qq.com/user/get_user_info?oauth_consumer_key=YOUR%s&openid=%s";
+	private static final String URL_GET_USER_INFO = "https://graph.qq.com/user/get_user_info?oauth_consumer_key=%s&openid=%s";
 	
 	
 	private String appId;
--- yeaxu-security-core/src/main/java/com/yeaxu/security/core/social/qq/config/QQAutoConfig.java
+++ yeaxu-security-core/src/main/java/com/yeaxu/security/core/social/qq/config/QQAutoConfig.java
@@ -1,10 +1,17 @@
 package com.yeaxu.security.core.social.qq.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.social.SocialAutoConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.ConnectionSignUp;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
 
 import com.yeaxu.security.core.properties.QQProperties;
 import com.yeaxu.security.core.properties.SecurityProperties;
@@ -16,11 +23,40 @@ public class QQAutoConfig extends SocialAutoConfigurerAdapter {
 
 	@Autowired
 	private SecurityProperties securityProperties;
-	
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired(required = false)
+	private ConnectionSignUp connectionSignUp;
+
 	@Override
 	protected ConnectionFactory<?> createConnectionFactory() {
-		QQProperties qqProperties = securityProperties.getSocial().getQq();
-		return new QQConnectionFactory(qqProperties.getProviderId(), qqProperties.getAppId(), qqProperties.getAppSecret());
+		QQProperties qqConfig = securityProperties.getSocial().getQq();
+		return new QQConnectionFactory(qqConfig.getProviderId(), qqConfig.getAppId(), qqConfig.getAppSecret());
 	}
 
+	/**
+	 * 如果不写在这里面就会出现Spring注入InMemoryUsersConnectionRepository有情况 
+	 * //	@Bean
+	 *	//	public UsersConnectionRepository usersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+	 *	//		JdbcUsersConnectionRepository repository = new JdbcUsersConnectionRepository(dataSource, connectionFactoryLocator, Encryptors.noOpText());
+	 *	//		repository.setTablePrefix("yeaxu_");
+	 *	//		if(connectionSignUp != null) {
+	 *	//			repository.setConnectionSignUp(connectionSignUp);
+	 *	//		}
+	 *	//		return repository;
+	 *	//	}  可解决
+	 */
+	@Override
+	public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+		JdbcUsersConnectionRepository repository = new JdbcUsersConnectionRepository(dataSource,
+				connectionFactoryLocator, Encryptors.noOpText());
+		repository.setTablePrefix("yeaxu_");
+		if (connectionSignUp != null) {
+			repository.setConnectionSignUp(connectionSignUp);
+		}
+		return repository;
+	}
+	
 }
