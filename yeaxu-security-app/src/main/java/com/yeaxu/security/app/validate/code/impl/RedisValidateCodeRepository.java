07d831089af45236ddeb7307b2aefd8001978d17
pcitc <pcitc@pcitc-PC>
2017-11-30 17:34:45 +0800

    接口定义

--- yeaxu-security-app/src/main/java/com/yeaxu/security/app/validate/code/impl/RedisValidateCodeRepository.java
+++ yeaxu-security-app/src/main/java/com/yeaxu/security/app/validate/code/impl/RedisValidateCodeRepository.java
@@ -1,5 +1,9 @@
 package com.yeaxu.security.app.validate.code.impl;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.context.request.ServletWebRequest;
 
 import com.yeaxu.security.core.validate.code.ValidateCode;
@@ -8,22 +12,26 @@ import com.yeaxu.security.core.validate.code.ValidateCodeType;
 
 public class RedisValidateCodeRepository implements ValidateCodeRepository {
 
+	@Autowired
+	private RedisTemplate<Object, Object> redisTemplate;
+	
 	@Override
 	public void save(ServletWebRequest request, ValidateCode code, ValidateCodeType validateCodeType) {
-		// TODO Auto-generated method stub
+		redisTemplate.opsForValue().set(buildKey(request , validateCodeType), code, 30, TimeUnit.MINUTES);
+	}
 
+	private Object buildKey(ServletWebRequest request, ValidateCodeType validateCodeType) {
+		return null;
 	}
 
 	@Override
 	public ValidateCode get(ServletWebRequest request, ValidateCodeType validateCodeType) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void remove(ServletWebRequest request, ValidateCodeType validateCodeType) {
-		// TODO Auto-generated method stub
-
+		redisTemplate.delete(buildKey(request, validateCodeType));
 	}
 
 }
